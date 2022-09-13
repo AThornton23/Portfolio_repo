2c4d971b33de2abbfaa684b2b13b07fe5da935a4
Antonio Thornton <antoniothornton01@gmail.com>
2022-09-13 13:17:46 -0500

    Updated

--- script.js
+++ script.js
@@ -21,15 +21,14 @@ function reveal() {
 
 
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const header = document.querySelector('.main-header');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
+window.addEventListener('scroll', () => {
+    const scrollPos = window.scrollY;
+    if (scrollPos > 10 ) {
+        header.classList.add('scrolled');
+    }
+    else {
+        header.classList.remove('scrolled');
+    }
+});
